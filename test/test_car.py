43426901f9b2250f0163fbf7d994b81dae57dcdc
Ovie <dvdhariss@gmail.com>
2022-04-22 13:16:13 +0100

    Task 3

---
+++ test/test_car.py
@@ -0,0 +1,84 @@
+import unittest
+from engine.engine import Engine
+from serviceable import Serviceable
+from datetime import datetime
+from battery.battery import Battery
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+
+class testNubbinBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 5)
+        nubbinBattery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(nubbinBattery.needs_service())
+    
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        nubbinBattery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(nubbinBattery.needs_service())
+
+class testSpindlerBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        spindlerBattery = SpindlerBattery(current_date, last_service_date)
+
+        self.assertTrue(spindlerBattery.needs_service())
+    
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+        spindlerBattery = SpindlerBattery(current_date, last_service_date)
+
+        self.assertFalse(spindlerBattery.needs_service())
+
+class testCapuletEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 30001
+        last_service_mileage = 0
+        capuletEngine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertTrue(capuletEngine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 30000
+        last_service_mileage = 0
+        capuletEngine = CapuletEngine(current_mileage, last_service_mileage)
+        self.assertFalse(capuletEngine.needs_service())
+
+class testSternmanEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        warning_light_is_on = True
+        sternmanEngine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(sternmanEngine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+        warning_light_is_on = False
+        sternmanEngine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(sternmanEngine.needs_service())
+
+class testWilloughbyEngine(unittest.TestCase):
+    def test_engine_should_be_serviced(self):
+        current_mileage = 60001
+        last_service_mileage = 0
+        willoughbyEngine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertTrue(willoughbyEngine.needs_service())
+
+
+    def test_engine_should_not_be_serviced(self):
+        current_mileage = 6000
+        last_service_mileage = 0
+        willoughbyEngine = WilloughbyEngine(current_mileage, last_service_mileage)
+        self.assertFalse(willoughbyEngine.needs_service())
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
